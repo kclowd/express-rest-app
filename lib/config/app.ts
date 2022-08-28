e800aef7f2385d2d14b8ea605ff73790fa4c667c
kclowd <kachambers86@gmail.com>
2022-08-28 15:43:07 -0600

    Cleaning up interview-specific code

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -2,17 +2,14 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { CommonRoutes } from "../routes/common_routes";
-import { StockRoutes } from "../routes/stock_routes";
 
 class App {
    public app: express.Application;
-   private stock_routes: StockRoutes = new StockRoutes();
    private common_routes: CommonRoutes = new CommonRoutes();
 
    constructor() {
       this.app = express();
       this.config();
-      this.stock_routes.route(this.app);
       this.common_routes.route(this.app);
    }
 private config(): void {
--- lib/modules/stock/stock.model.ts
+++
@@ -1,9 +0,0 @@
-export class ValuationModel {
-    symbol: string
-    price: number
-
-    constructor(symbol: string, price: number) {
-        this.symbol = symbol;
-        this.price = price;
-    }
-}
